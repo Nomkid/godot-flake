a4cfccd189db074316b938f72a27b3c4cb65a38d
Jeeves <guydoodlesdev@gmail.com>
2024-08-15 20:23:59 -0600

    move to overriding nixpkgs attrs

--- flake.nix
+++ flake.nix
@@ -10,64 +10,60 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
 
-      version = "4.3-stable";
-      godot-stable = pkgs.fetchurl {
-        url = "https://github.com/godotengine/godot-builds/releases/download/${version}/Godot_v${version}_linux.x86_64.zip";
-        hash = "sha256-feVkRLEwsQr4TRnH4M9jz56ZN+5LqUNkw7fdEUJTyiE=";
-      };
+      # version = "4.3-stable";
+      # godot-stable = pkgs.fetchurl {
+      #   url = "https://github.com/godotengine/godot-builds/releases/download/${version}/Godot_v${version}_linux.x86_64.zip";
+      #   hash = "sha256-feVkRLEwsQr4TRnH4M9jz56ZN+5LqUNkw7fdEUJTyiE=";
+      # };
+
+      commitHash = "77dcf97d82cbfe4e4615475fa52ca03da645dbd8";
 
       buildInputs = with pkgs; [
-        alsa-lib
-        dbus
-        fontconfig
-        libGL
-        libpulseaudio
-        libxkbcommon
-        makeWrapper
-        mesa
-        patchelf
-        speechd
-        udev
-        vulkan-loader
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXext
-        xorg.libXfixes
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXrandr
-        xorg.libXrender
         wayland-utils
         wayland-scanner
         libdecor
       ];
 
-      godot-unwrapped = pkgs.stdenv.mkDerivation {
-        pname = "godot";
-        version = "4.3-beta1";
+      godot-bin = pkgs.godot_4.overrideAttrs (prev: {
+        version = "4.3.0";
+        inherit commitHash;
+        src = pkgs.fetchFromGitHub {
+          owner = "godotengine";
+          repo = "godot";
+          rev = commitHash;
+          hash = "sha256-v2lBD3GEL8CoIwBl3UoLam0dJxkLGX0oneH6DiWkEsM=";
+        };
 
-        src = godot-stable;
-        nativeBuildInputs = with pkgs; [unzip autoPatchelfHook];
-        buildInputs = buildInputs;
+        buildInputs = buildInputs ++ prev.buildInputs;
+        runtimeDependencies = buildInputs ++ prev.runtimeDependencies;
+      });
 
-        dontAutoPatchelf = false;
+      # godot-unwrapped = pkgs.stdenv.mkDerivation {
+      #   pname = "godot";
+      #   version = "4.3-beta1";
 
-        unpackPhase = ''
-          mkdir source
-          unzip $src -d source
-        '';
+      #   src = godot-stable;
+      #   nativeBuildInputs = with pkgs; [unzip autoPatchelfHook];
+      #   buildInputs = buildInputs;
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp source/Godot_v${version}_linux.x86_64 $out/bin/godot
-        '';
-      };
+      #   dontAutoPatchelf = false;
 
-      godot-bin = pkgs.buildFHSUserEnv {
-        name = "godot";
-        targetPkgs = pkgs: buildInputs ++ [godot-unwrapped];
-        runScript = "godot";
-      };
+      #   unpackPhase = ''
+      #     mkdir source
+      #     unzip $src -d source
+      #   '';
+
+      #   installPhase = ''
+      #     mkdir -p $out/bin
+      #     cp source/Godot_v${version}_linux.x86_64 $out/bin/godot
+      #   '';
+      # };
+
+      # godot-bin = pkgs.buildFHSUserEnv {
+      #   name = "godot";
+      #   targetPkgs = pkgs: buildInputs ++ [godot-unwrapped];
+      #   runScript = "godot";
+      # };
     in {
       packages.default = godot-bin;
 
